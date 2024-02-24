755c8487e066c46d6b0a576249477636d117fbc5
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-02-24 13:47:38 +0530

    free demo, calendly page fixed

--- frontend/src/views/Calandly/Calandly.jsx
+++ frontend/src/views/Calandly/Calandly.jsx
@@ -13,17 +13,14 @@ function About() {
       <Nav />
       <section className={styles["demo-page"]}>
       <div className="App">
-      <PopupWidget
+      {/* <PopupWidget
         url="https://calendly.com/xperiokids"
-        /*
-         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-         */
         rootElement={document.getElementById("root")}
         text="Click here to schedule a free Demo!"
         textColor="#ffffff"
         color="#21654E"
-      />
+        
+      /> */}
     </div>
         <div className={styles.about}>
           {/* <img src={pic} className={styles.pic} alt="Profile Picture" /> */}
@@ -33,27 +30,27 @@ function About() {
               <span>More details on the Free Demo! Please read them before scheduling</span>
             </h5>
             <p>
-              1) The Slot you select could be changed based on Students Grade and our faculty's availability. We try
-our best to give everyone the same slots as chosen. However, we also try to make the best of our time
-and optimize. We will inform you over a phone call and give you options to choose from!
+              1) The Slot you select could be changed based on Student's Grade and our faculty's availability. We try
+our best to give everyone the same slots as chosen. We will inform you over a phone call and give you options to choose from!
             </p>
             <br/>
             <p>
               2) You are free to bring along other students for the free demo (upto 4 additional friends). However,
-Pease inform us in the form or via a phone call or whatsapp so we can plan accordingly. We do not want
-the room to become so crowed.
+Please inform us in the form or via a phone call or whatsapp so we can plan accordingly. Students should carry a notebook and a pen 
+to take any notes. Please arrive on time. We do not have any car parking, so request parents to use the parking by the
+roadside. Shubh enclave gives enough free parking and is only 2 minutes walk away
             </p>
             <br/>
             <p>
-              3) The Demo will be as per the grade selected. However, pelase note that a few experiments may be
-from a senior/junior class due to the nature of Science. We however try our best to give a good learning
-from the demo.
+              3) The Demo experiments are using harmless chemicals. Safety is always our first priority. 
             </p>
+            <br/>
+            <p>
+              4) By attending the Demo, you agree to our terms and conditions as mentioned on our website page.
+            </p>
+            <br/>
             <div className={styles.data}>
-              <a href="https://www.linkedin.com/in/kislaysingh/" target="_blank" className={styles.hire}>
-              <ion-icon name="logo-linkedin"></ion-icon>
-                 LinkedIn Profile
-              </a>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdAIXacuRObeGgnT0QN3jW8x2SU5qijyUDNHepZZbpZzZR3Ig/viewform?embedded=true" width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
           </div>
         </div>
@@ -64,24 +61,3 @@ from the demo.
 }
 
 export default About;
-
-
-// function Calandly() {
-//   return (
-//     <div className="App">
-//       <PopupWidget
-//         url="https://calendly.com/xperiokids"
-//         /*
-//          * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-//          * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-//          */
-//         rootElement={document.getElementById("root")}
-//         text="Click here to schedule a free Demo!"
-//         textColor="#ffffff"
-//         color="#21654E"
-//       />
-//     </div>
-//   );
-// };
-
-// export default Calandly;
