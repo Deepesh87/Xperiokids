ebff44f4417a816577502a7e87e4c0f6d240e3f4
shivaay044 <shivendrapaliwal99990@gmail.com>
2024-02-25 20:19:07 +0530

    empty commit

--- frontend/src/views/Calandly/Calandly.jsx
+++ frontend/src/views/Calandly/Calandly.jsx
@@ -6,10 +6,6 @@ function Calandly() {
     <div className="App">
       <PopupWidget
         url="https://calendly.com/xperiokids"
-        /*
-         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-         */
         rootElement={document.getElementById("root")}
         text="Click here to schedule a free Demo!"
         textColor="#ffffff"
