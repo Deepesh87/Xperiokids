0c0070d10b17e32f4f725d5f27842029c806f128
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-03-11 23:13:18 +0530

    txt

--- frontend/src/views/LandingPg/WhyXperio.jsx
+++ frontend/src/views/LandingPg/WhyXperio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Style from "./WhyXperio.module.css";
 import lab from "../../assets/lab.jpg";
+import goals from "../../assets/goals.jpg";
 
 function WhyXperio() {
   return <div className={Style.main}>
@@ -25,26 +26,8 @@ function WhyXperio() {
            grades 11-12, as well as those preparing for Medical | Engineering entrance exams, we offer specialized Chemistry & Biology classes.
            Our Tuitons also include a lot of practical experiments in addition to theory. We also provide 1:1 classes on request.</p>
       </div>
-      <img  width={"100%"} height={"100%"} src="https://img.freepik.com/free-vector/reading-achieve-goals-education-success-concept-vector-flat-illustration-man-with-flag-award-standing-top-books-stack-staircase-books-leading-win_107791-11835.jpg?size=626&ext=jpg&uid=R135795550&ga=GA1.1.905453276.1706342616&semt=ais" alt="toghther"/>
+      <img  width={"100%"} height={"100%"} src={goals} alt="toghther"/>
     </div>
-    {/* <div className={Style.together} >
-      <img  width={"100%"} height={"100%"} src="https://img.freepik.com/free-vector/career-path-concept-illustration_114360-13664.jpg?size=626&ext=jpg&uid=R135795550&ga=GA1.1.905453276.1706342616&semt=ais" alt="toghther"/>
-      <div id={Style.txt}>
-         <strong>LESSONS FEATURING REAL PEOPLE</strong>
-         <h2>Learn for real life</h2>
-         <p>Say goodbye to outdated phrases and hello to skills that take you places. Learn language for every day with regularly updated content, video flashcards with real people and helpful cultural insights.</p>
-      </div>
-    </div>
-    <div className={Style.together} id={Style.reverse_img}>
-      <div id={Style.txt} >
-         <strong>SELF-LED LEARNING</strong>
-         <h2>Learn your way</h2>
-         <p>Whatever your reason for learning a language, we’ve got you covered. Learn for any goal with simplified courses and short, but focused lessons. You’re in control.</p>
-      </div>
-      <img width={"100%"} height={"100%"} src="https://img.freepik.com/free-vector/flat-university-concept-background_23-2148189344.jpg?size=626&ext=jpg&uid=R135795550&ga=GA1.1.905453276.1706342616&semt=ais" alt="toghther"/>
-    </div>
-  </div>; */}
-
 </div>;
 }
 
