934f119537bb63cb601656e4a12fc4eb9ca84a32
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-11-17 11:46:14 +0530

    remove comments

--- frontend/src/views/LandingPg/Landing.jsx
+++ frontend/src/views/LandingPg/Landing.jsx
@@ -7,29 +7,6 @@ function Landing() {
   return (
     <>
       <div className={Style.main}>
-        {/* <div className={Style.content}>
-          <div className={Style.textContent}>
-            <h1>
-              <span>
-                Xperiment,<br /> Xperience
-                {
-                  <Typewriter
-                    options={{
-                      strings: [
-                        '<span style="color: #21654E;">Science!</span>',
-                        '<span style="color: #21654E;">Robotics!</span>',
-                        '<span style="color: #21654E;">Coding!</span>',
-                        '<span style="color: #21654E;">Public Speaking!</span>',
-                      ],
-                      autoStart: true,
-                      loop: true,
-                    }}
-                  />
-                }{" "}
-              </span>
-            </h1>
-            <p>Join us today and witness the positive change in your child!</p>
-          </div> */}
           <div className={Style.content}>
             <div>
               <h1>
