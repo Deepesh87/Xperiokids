a9aa646306573dfcd94a751a1da4313083781dee
deepesh87 <deepesh123123@gmail.com>
2025-02-22 23:37:47 +0530

    camp page set

--- frontend/src/views/summercamp/camp.jsx
+++ frontend/src/views/summercamp/camp.jsx
@@ -11,7 +11,9 @@ function Camp() {
       <section className={styles["demo-page"]}>
         <div className={styles.about}>
           <div className={styles.text}>
-            <h5><span>Summer Camp 2025:</span></h5>
+            <h5>
+              <span>Summer Camp 2025:</span>
+            </h5>
 
             <img src={camp_new} className={styles.pic} alt="Summer Camp 2025" />
 
@@ -27,38 +29,39 @@ function Camp() {
             </p>
 
             <div className={styles.videoContainer}>
-            <div className={styles.videoWrapper}>
-              <iframe
-                src="https://www.youtube.com/embed/BgBgUHmVf7s"
-                title="Xperio Learning Summer Camp 2024 Feedback"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-            <div className={styles.videoWrapper}>
-              <iframe
-                src="https://www.youtube.com/embed/HL50OhKD9HM"
-                title="Xperio Learning Summer Camp 2024 Feedback"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
+              <div className={styles.videoWrapper}>
+                <iframe
+                  src="https://www.youtube.com/embed/BgBgUHmVf7s"
+                  title="Xperio Learning Summer Camp 2024 Feedback"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+              <div className={styles.videoWrapper}>
+                <iframe
+                  src="https://www.youtube.com/embed/HL50OhKD9HM"
+                  title="Xperio Learning Summer Camp 2024 Feedback"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
             </div>
 
-            
             <p>
-              View photos & activities from last year's camp:   
-              <a className={styles.menu__link} href="/camp24" target="_blank" rel="noopener noreferrer"> Summer Camp 2024</a>.
+              View photos & activities from last year's camp:{" "}
+              <a className={styles.menu__link} href="/camp24" target="_blank" rel="noopener noreferrer">
+                Summer Camp 2024
+              </a>.
             </p>
 
             <p>
-              To join an exclusive Robotics Camp during Summer Vacation, please visit here   
-              <a className={styles.menu__link} href="/robotics" target="_blank" rel="noopener noreferrer"> Robotics Summer Camp</a>.
+              To join an exclusive Robotics Camp during Summer Vacation, please visit here{" "}
+              <a className={styles.menu__link} href="/robotics" target="_blank" rel="noopener noreferrer">
+                Robotics Summer Camp
+              </a>.
             </p>
-
-
           </div>
         </div>
       </section>
