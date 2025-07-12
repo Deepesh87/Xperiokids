a21465f6829143e0a83f88873f4a15b3675b12fa
deepesh87 <deepesh123123@gmail.com>
2025-07-12 21:06:31 +0530

    many changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
-import MainRoutes from './routes';
 import ScrollToTop from './views/ScrollToTop';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import MainRoutes from './routes';
 
 function App() {
   return (
