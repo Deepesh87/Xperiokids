43c23b88c14358f6ed4da7eb6b5e9afdf77643fa
deepesh87 <deepesh123123@gmail.com>
2025-10-13 23:10:44 +0530

    update woird count

--- frontend/src/views/script_evaluator/ScriptEvaluator.jsx
+++ frontend/src/views/script_evaluator/ScriptEvaluator.jsx
@@ -200,7 +200,7 @@ const handleEvaluate = async () => {
       <span className={styles.lineText}>Okay to submit for faculty review.</span>
     </li>
     <li>
-      <span className={styles.lineText}>A 3- to 5-minute speech should be between 400 and 800 words, depending on your speaking speed.</span>
+      <span className={styles.lineText}>A 3- to 5-minute speech should be between 500 and 800 words, depending on your speaking speed.</span>
     </li>
   </ul>
 </div>
